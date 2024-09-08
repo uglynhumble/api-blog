60ca2054d831f4b0b84bc5fad0a9a7bed0c45c95
uglynhumble <platon.luk.98@mail.ru>
2024-09-08 18:58:02 +0300

    feat/update crud controller

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post } from '@nestjs/common';
 import { PostsService } from './posts.service';
+import { CreatePostDto } from './dto/createPost.dto';
 
 @Controller()
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
+  @Post('add')
+  CreatePost(createPostDto: CreatePostDto) {
+    return this.postsService.CreatePost(createPostDto);
+  }
+
+  @Get('id')
+  getPost(id: number) {
+    return this.postsService.getPost(id);
+  }
+
+  // @Patch('edit')
+  // editPost(updatePostDto: RequestPostDto) {
+  //   return this.postsService.editPost(updatePostDto);
+  // }
+  @Delete('remove')
+  deletePost(id: number) {
+    return this.postsService.deletePost(id);
+  }
 }
